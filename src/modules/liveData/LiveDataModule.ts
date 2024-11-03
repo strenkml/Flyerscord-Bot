45730c04ff7c84259dd0f5e2d212ac4f829a61b2
default <mstrenk12@gmail.com>
2024-11-02 21:01:51 -0400

    Add module frame

---
+++ src/modules/liveData/LiveDataModule.ts
@@ -0,0 +1,11 @@
+import Module from "../../common/models/Module";
+
+export default class LiveDataModule extends Module {
+  constructor() {
+    super("LiveData");
+  }
+
+  protected override async setup(): Promise<void> {
+    // TODO: Implement
+  }
+}
